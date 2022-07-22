d2b1f4c66f02579bab40c0cbb8b9c9182362300b
islomadbuqodir <islomadbuqodir@gmail.com>
2022-07-22 17:03:37 +0500

    ecofruiit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
 
-	MicroModal.init({
-		openTrigger: 'data-custom-open',
-		closeTrigger: 'data-custom-close',
-		disableScroll: true,
-		disableFocus: true,
-		awaitOpenAnimation: true,
-		awaitCloseAnimation: true
-	})
-	$('[data-custom-open]').click(function() {
-		$('.modal [name=form]').val($(this).data('form'))
-	})
-	$('[data-custom-close]').click(function() {
-		$('.modal [name=form]').val('')
-	})
-
-	// callback list numbers
-	$('.home-callback__list li').each(function () {
-		$(this).html($(this).text().replace(/([0-9.]+)/g, `<span>$1</span>`))
-	})
-
-})
 
 // burger menu
 
